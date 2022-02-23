cee8de7b1b1ed073fa3f0560997a4df270dcb238
preethi214 <97090251+preethi213@users.noreply.github.com>
2022-02-23 06:01:53 +0000

    done

--- p1original.c
+++ p1original.c
@@ -7,7 +7,7 @@ void input(float *x1,float *y1,float *x2,float *y2)
 }
 void find_distance(float x1,float y1,float x2,float y2,float *distance)
 {
-  *distance=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+  *distance= sqrt(pow((x2-x1),2)+pow((y2-y1),2));
   }
 void output(float x1,float y1,float x2,float y2,float distance)
 {
